75f94bd9ee85291e5a72777c7135933c52fda617
janusz <janusz.agh@gmail.com>
2020-04-21 21:19:50 +0200

    variables

---
+++ pt01/t01.js
@@ -0,0 +1,9 @@
+var name = "Maria Jones";
+var age = 21;
+var hasDriverLicence = true;
+var unused = undefined;
+
+console.log(name + " type = " + typeof name);
+console.log(age + " type = " + typeof age);
+console.log(hasDriverLicence + " type = " + typeof hasDriverLicence);
+console.log(unused + " type = " + typeof unused);
