ac8ae2c13dd1c57905a95036fb5629752ad1001e
janusz <janusz.agh@gmail.com>
2020-04-22 16:48:59 +0200

    objects

--- pt01/t02.js
+++ pt01/t02.js
@@ -1,7 +1,12 @@
 var person = {
     name: "Maria Jones",
     age: 21,
-    hasDriverLicence: true
+    hasDriverLicence: true,
+    dateOfBirth: "01/01/2000",
+    address: {
+        city: "Warsaw",
+        street: "Wrzeciono"
+    }
 };
 
 console.log(person);
@@ -11,3 +16,10 @@ console.log(JSON.stringify(person));
 console.log(person.name);
 console.log(person.age);
 console.log(person.hasDriverLicence);
+console.log(person.dateOfBirth);
+console.log(person.address.city);
+console.log(person.address.street);
+
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
