3cd368b683933126938c199ca777060cc7c0bba8
janusz <janusz.agh@gmail.com>
2020-04-22 19:30:16 +0200

    arrays for of

--- pt01/t03.js
+++ pt01/t03.js
@@ -1,2 +1,25 @@
 var names = ["Alex", "Maria", "Sam"];
-console.log(names);
+// console.log(names);
+
+
+// for (var name of names) {
+//     console.log(name);
+// }
+//
+// console.log(name);
+
+var person = {
+    name: "Maria Jones",
+    age: 21,
+    hasDriverLicence: true,
+    dateOfBirth: "01/01/2000",
+    address: {
+        city: "Warsaw",
+        street: "Wrzeciono"
+    }
+};
+
+for (var p in person) {
+    console.log(p, " ", person[p]);
+}
+
