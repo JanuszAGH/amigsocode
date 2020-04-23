f816b233f52b38bc5ec0e7122404cca8f932e481
janusz <janusz.agh@gmail.com>
2020-04-23 19:00:38 +0200

    var let

---
+++ pt01/t07.js
@@ -0,0 +1,13 @@
+var condition;
+
+if (Math.random() < 0.5) {
+    condition = false;
+} else {
+    condition = true;
+}
+
+if (condition) {
+    console.log("yea");
+} else {
+    console.log("nay");
+}
