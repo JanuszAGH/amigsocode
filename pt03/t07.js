db11e707f08dc2e6ed6aace6be63e6e5213b12f4
janusz <janusz.agh@gmail.com>
2020-04-24 21:45:19 +0200

    array descructor

---
+++ pt03/t07.js
@@ -0,0 +1,12 @@
+const names = ['Anna', "Miriam", 'Joe', 'Mark', 'Matt'];
+
+const [a, b, c] = names;
+console.log(a);
+console.log(b);
+console.log(c);
+
+const offer = ["Ford", "Mustang", 2015];
+const [make, model, year] = offer;
+console.log(make);
+console.log(model);
+console.log(year);
