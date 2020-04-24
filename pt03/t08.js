321de6a5144e6c487e74b4963f11bca4db41f2eb
janusz <janusz.agh@gmail.com>
2020-04-24 22:36:53 +0200

    array descructor

---
+++ pt03/t08.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: "Mark",
+    lastName: "Jockey",
+    age: 24
+}
+
+const {firstName, lastName, age} = person;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
