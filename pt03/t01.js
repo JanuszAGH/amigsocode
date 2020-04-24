acf908f47a307d093b42b218502a9ab50d4341d4
janusz <janusz.agh@gmail.com>
2020-04-24 16:53:39 +0200

    functions

---
+++ pt03/t01.js
@@ -0,0 +1,16 @@
+const person = {}
+
+person.name = "Jason";
+
+console.log(JSON.stringify(person));
+
+var fname = "Maria";
+console.log(fname);
+fname = 1;
+console.log(fname);
+
+const array = [];
+array.unshift(1);
+array.unshift(2);
+array.unshift(3);
+console.log(array);
