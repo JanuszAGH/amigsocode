fd48d8c6ca66d2ff100919c6fbdd30dc6048119b
janusz <janusz.agh@gmail.com>
2020-04-24 20:16:46 +0200

    this

--- pt03/t03.js
+++ pt03/t03.js
@@ -1,33 +1,11 @@
-const arrOne = ["Marian", "Anna", "Alex"];
-const arrTwo = ['Said', 'Ismail', 'Aisha'];
-
-const arrThree = [...arrOne, ...arrTwo];
-
-arrThree.forEach(e => console.log(e));
-
-function sum(a, b, c) {
-    return a + b + c;
-}
-
-const numbers = [1, 2, 3];
-
-let s = sum(...numbers);
-console.log(s);
-
-const name = {
-    firstName: "Andy",
-    lastName: "Jones"
-}
-
-const address = {
-    country: "Poland",
-    city: "Warszawa",
-    street: "Wrzeciono"
-}
-
 const person = {
-    ...name,
-    ...address
+    name: 'Alex',
+    cars: ['Ferrari', 'Lambo'],
+    toString: function () {
+        this.cars.forEach(function (c) {
+            console.log(`${this.name} has ${c}`)
+        }.bind(this));
+    }
 }
 
-console.log(person);
+person.toString();
