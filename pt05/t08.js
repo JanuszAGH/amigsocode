445a9f21d1694182eead1a0b60c6842b6a01bfe2
janusz <janusz.agh@gmail.com>
2020-04-26 21:53:40 +0200

    currying

--- pt05/t08.js
+++ pt05/t08.js
@@ -21,3 +21,26 @@ let message = curry("Tom")("hello")("Jerry");
 console.log(message);
 
 
+const offer = ["Ford", "Mustang", 2015];
+const [make, model, year] = offer;
+console.log(make);
+console.log(model);
+console.log(year);
+
+const person = {
+    name: "Ann",
+    sex: "F"
+};
+
+const address = {
+    country: "Poland",
+    city: "Warsaw",
+    street: "Kasprowicza"
+};
+
+const info = {
+    ...person,
+    ...address
+};
+
+console.log(info);
