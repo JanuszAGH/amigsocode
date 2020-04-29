023a58a4e5cc94eec803d2e69c7a19e38734309b
janusz <janusz.agh@gmail.com>
2020-04-29 20:22:38 +0200

    promise all

--- pt05/t025.js
+++ pt05/t025.js
@@ -8,5 +8,17 @@ const evens = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve([2, 4, 6, 8, 10]);
     }, 2000);
+
+    setTimeout(() => {
+        reject([]);
+    }, 1000);
 });
 
+// evens
+//     .then((data) => console.log(data));
+// odds
+//     .then((data) => console.log(data));
+
+Promise.all([odds, evens])
+    .then(data => console.log(data))
+    .catch((d) => console.log('fackup'));
